8232fa694ef3dec4f20997f398f5c0c0f20ad024
Peter Rake <par@init4.de>
2022-01-22 23:02:26 +0100

    Add files via upload

---
+++ ui/StartScreen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _input(event):
+	if event.is_action_pressed('ui_select'):
+		Global.new_game()
+		
+func _ready():
+	$ScoreNotice.text = "High Score: " + str(Global.highscore)
+	
